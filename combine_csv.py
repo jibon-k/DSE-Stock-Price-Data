a0810d318d342936243f1fc06be3765a1616714a
JibonKrishna <jibon33.ml@gmail.com>
2021-09-26 20:20:40 +0600

    splited the combined csv into two.

--- combine_csv.py
+++ combine_csv.py
@@ -23,8 +23,10 @@ for n, csv in enumerate(all_csv):   # enumerate the list of csv
     # create dictionary of dataframes as the values and the indices as keys.
     dic[n] = make_dataframe(f'csv/{csv}')
 
-# combine all the dataframes to one
-combined_df = pd.concat([dic[i] for i in range(len(dic))])
+# combine all the dataframes
+combined_df1 = pd.concat([dic[i] for i in range(0, 11)])
+combined_df2 = pd.concat([dic[i] for i in range(11, 22)])
 
 # export the combined dataframe as csv
-combined_df.to_csv('combined.csv', index=False)
+combined_df1.to_csv('1999-2009.csv', index=False)
+combined_df2.to_csv('2010-2020.csv', index=False)
